c49a0ff9a15fac59c08939104a029a8f0a7ffb06
nexusonez <asonkamlol88@gmail.com>
2022-12-25 01:22:54 +0800

    Controller and view update
    feat - got add some controller in lecturer POV and student POV.  what methods they are going to use.
    feat - added new attributes

---
+++ Controller/StudentController.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import Model.Project;
+import View.Viewboard;
+
+public class StudentController {
+    private Project projectModel;
+    private Viewboard viewboard;
+    
+    public StudentController(Project projectModel, Viewboard viewboard) {
+        super();
+        this.projectModel = projectModel;
+        this.viewboard = viewboard;
+    }
+
+    public void favouriteSubject(){
+        if(projectModel.getFavourite() == false){
+            projectModel.setFavourite(true);
+        }else{
+            projectModel.setFavourite(false);
+        }
+        updateView();
+    }
+
+    public void updateView(){
+        viewboard.printListOfProjects(projectModel);
+    }
+
+}
