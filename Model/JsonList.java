34081cbaf57e5c1134616ca578220414e5081904
YBZach <boonzhe.yaw@gmail.com>
2022-12-31 15:09:26 +0800

    docs: add documentation in jsonList

--- Model/JsonList.java
+++ Model/JsonList.java
@@ -3,11 +3,15 @@ package Model;
 import java.util.ArrayList;
 
 public  interface JsonList<T> {
+    // populates ArrayList with all the file's data
     public void setList(); 
 
+    // saves the list to the json file
     public void save();
 
+    // adds new items to the ArrayList
     public void addItem(T item);
 
+    // get specified item from ArrayList by the item's id 
     public T getItem(String id);
 }
