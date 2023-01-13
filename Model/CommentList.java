878d6bdbdf84523264b59d92af2f386a5793a8c7
nexusonez <asonkamlol88@gmail.com>
2023-01-13 15:14:46 +0800

    feat: java docs for commentList and Model

--- Model/CommentList.java
+++ Model/CommentList.java
@@ -2,18 +2,24 @@ package Model;
 
 import java.util.ArrayList;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.File;
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * A list of comments will have several comments from comments.json which implements a parser from JsonList.java
+ * @author Kam Kar Hou
+ * @version 1.0
+ */
 public class CommentList implements JsonList<CommentModel> {
     JsonParser<CommentModel> parser = new JsonParser<>("/Database/comments.json", CommentModel.class);
     private ArrayList<CommentModel> comments;
 
+    /**
+     * A default constructors that creates an arraylist object with the model of CommentModel.java
+     * and reads the comments.java file into the arraylist comments.
+     */
     public CommentList(){
         try{
             this.comments = parser.deserialize();
@@ -21,14 +27,18 @@ public class CommentList implements JsonList<CommentModel> {
             e.printStackTrace();
         }
     }
-    //CommentList will contain all the comments of the project
-    public CommentList(Project project){
+    /**
+     * A contructor that filters out the projects which are not the current project
+     * to see the current project's comments
+     * @param currentProject represents the project that is current viewed
+     */
+    public CommentList(Project currentProject){
         try {
             this.comments = parser.deserialize();
             for (int i = 0; i < comments.size(); i++){
                 CommentModel comment = comments.get(i);
                 String projectID = comment.getProjectID();
-                if (!(projectID.equals(project.getId()))){
+                if (!(projectID.equals(currentProject.getId()))){
                     comments.remove(i);
                     i--;
                 }
@@ -38,26 +48,22 @@ public class CommentList implements JsonList<CommentModel> {
         }    
     }
 
+    /**
+     * Getter for Arraylist of comments
+     * @return Arraylist of comments
+     */
     public ArrayList<CommentModel> getComments(){
         return comments;
     }
+    /**
+     * This is to return a comment id with the format 0001 to 9999
+     * to be added with a string "C" to produce a unique ID e.g. C0001
+     * @return Comment Id Number
+     */
     public String generateCommentIdNum(){
         return String.format("%04d", comments.size() + 1);
     }
-    public void writeAllData(CommentModel listComments){
-        ObjectMapper om = new ObjectMapper();
 
-        try{
-            om.writerWithDefaultPrettyPrinter().writeValue(new File("Database/comments.json"), listComments);;
-        }catch(JsonGenerationException e){
-            e.printStackTrace();
-        }catch(JsonMappingException e){
-            e.printStackTrace();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-    }
-    // For adding new json data
     @Override
     public void addItem(CommentModel item) {
         this.comments.add(item);
@@ -68,6 +74,10 @@ public class CommentList implements JsonList<CommentModel> {
         }
     }
 
+    /**
+     * A getter for a specific comment id in an arraylist of comments
+     * @return comment
+     */
     @Override
     public CommentModel getItem(String id) {
         for (int i = 0; i < comments.size(); i++){
@@ -78,11 +88,16 @@ public class CommentList implements JsonList<CommentModel> {
         }
         return null;
     }
+
     @Override
     public void setList() {
         // TODO Auto-generated method stub
         
     }
+    
+    /**
+     * A method that saves the commentList into the database comments.json
+     */
     @Override
     public void save() {
         // Write comments to file using JSON parser
@@ -101,19 +116,6 @@ public class CommentList implements JsonList<CommentModel> {
         // TODO Auto-generated method stub
         return 0;
     }
-
-    //TEST DATA ENTRY
-    
-    // public static void main(String[] args){
-    //     Lecturer lec = new Lecturer("L007", "Mickey Deez", "DeezNutz123");
-    //     Student stu = new Student("S001", "Goofy Ass", "HoeDuck", "Hotel Management", "P001");
-    //     User user = new User("L0007","Danish","Abc123");
-    //     Project pro = new Project("P001","Manage Hotel", "Hotel Management", "Assign people to love hotel room 1001", lec, stu, true, true, false);
-
-    //     CommentModel c1 = new CommentModel("C0001", pro,user,"Sir do we have class tmrw?" );
-    //     CommentList cl = new CommentList();
-    //     cl.writeAllData(c1);
-    // }
 
 }
 
